9deb9387ef99fcb4d94da9cbf3c2527806e65624
bradley simmons <bradleysimmons9@gmail.com>
2016-09-14 15:49:28 -0500

    add allergy check

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,93 +14,128 @@ until current_count == users
 
 # write a program that asks the new employee the following questions.
 
-puts "what is your name?"
-name = gets.chomp
+  puts "what is your name?"
+  name = gets.chomp
 
-puts "how old are you?"
-age = gets.chomp.to_i
+  puts "how old are you?"
+  age = gets.chomp.to_i
 
-puts "what year were you born?"
-birth_year = gets.chomp.to_i
+  puts "what year were you born?"
+  birth_year = gets.chomp.to_i
 
-puts "should we order you some garlic bread?"
-garlic = gets.chomp
+  puts "should we order you some garlic bread?"
+  garlic = gets.chomp
 
-puts "would you like to enroll in our health care plan?"
-health_plan = gets.chomp
+  puts "would you like to enroll in our health care plan?"
+  health_plan = gets.chomp
 
-# set conditional for checking truth on age
+  #In addition to the other questions in the survey, use a loop to ask 
+  #the employee to name any allergies, one at a time. The employee can 
+  #type “done” when finished.
 
-current_year = 2016
-if age == current_year - birth_year
-  age_truth = true
-elsif age == current_year - birth_year - 1
-  age_truth = true
-else
-  age_truth = false
-end
+  #As long as the allergy is not “sunshine,” continue the loop for as 
+  #long as is needed. If at any point the employee lists “sunshine” as 
+  #an allergy, skip directly to the result of “Probably a vampire.”
 
-# set booleans for garlic and health plan
+  # my notes: set up a loop that stops when user types "done" or "sunshine"
+  #           if user types "done", process results
+  #           if user types "sunshine", store result as "probably a vampire"
+  
+  puts "please enter your allergies, one at a time."  
+  puts "once you are finished, or if you do not have any allergies," 
+  puts "please enter 'done'"
+  
+  while true
+    input = gets.chomp
+    if input == "sunshine"
+      break
+    elsif input == "done"
+      break
+    end
+  end
 
-if garlic == "yes"
-  garlic_willing = true
-else
-  garlic_willing = false
-end
+  if input == "sunshine"
+    result = "probably a vampire"
+  end
 
-if health_plan == "yes"
-  health_plan_willing = true
-else
-  health_plan_willing = false
-end
+  # skip survey if result = "probably a vampire"
 
-# set booleans for drake cula and tu fang
+  if result != "probably a vampire"
 
-if name == "tu fang" || name == "drake cula"
-  def_vampire = true
-else
-  def_vampire = false
-end
+    # set booleans for checking truth on age
+
+    current_year = 2016
+    if age == current_year - birth_year
+      age_truth = true
+    elsif age == current_year - birth_year - 1
+      age_truth = true
+    else
+      age_truth = false
+    end
+
+    # set booleans for garlic and health plan
+
+    if garlic == "yes"
+      garlic_yes = true
+    else
+      garlic_yes = false
+    end
+
+    if health_plan == "yes"
+      health_yes = true
+    else
+      health_yes = false
+    end
+
+    # set booleans for drake cula and tu fang
+
+    if name == "tu fang" || name == "drake cula"
+      def_vamp = true
+    else
+      def_vamp = false
+    end
 
 
-# If the employee got their age right, and is willing to eat garlic bread 
-# or sign up for insurance, the result is “Probably not a vampire.”
+    # If the employee got their age right, and is willing to eat garlic bread 
+    # or sign up for insurance, the result is “Probably not a vampire.”
 
-if (def_vampire == false) && age_truth && (garlic_willing || health_plan_willing) 
-  result = "probably not a vampire"
-
-
-# If the employee got their age wrong, and hates garlic bread or waives insurance, 
-# the result is “Probably a vampire.”
-
-elsif (def_vampire == false) && (age_truth == false) && ((garlic_willing == false) || (health_plan_willing == false))
-  result = "probably a vampire"
+    if (def_vamp == false) && age_truth && (garlic_yes || health_yes) 
+      result = "probably not a vampire"
 
 
-# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, 
-# the result is “Almost certainly a vampire.”
+    # If the employee got their age wrong, and hates garlic bread or waives insurance, 
+    # the result is “Probably a vampire.”
 
-elsif (def_vampire == false) && (age_truth == false) && (garlic_willing == false) && (health_plan_willing == false)
-  result = "almost certainly a vampire"
+    elsif (def_vamp == false) && (age_truth == false) && ((garlic_yes == false) || (health_yes == false))
+      result = "probably a vampire"
 
-# Even if the employee is an amazing liar otherwise, anyone going by the name of 
-# “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, 
-# you should print “Definitely a vampire.”
 
-elsif (def_vampire == true)
-  result = "definitely a vampire"
+    # If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, 
+    # the result is “Almost certainly a vampire.”
 
-# Otherwise, print “Results inconclusive.”
+    elsif (def_vamp == false) && (age_truth == false) && (garlic_yes == false) && (health_yes == false)
+      result = "almost certainly a vampire"
 
-else
-  result = "results inconclusive"
-end
+    # Even if the employee is an amazing liar otherwise, anyone going by the name of 
+    # “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, 
+    # you should print “Definitely a vampire.”
 
-# Print the result at the end of the survey.
+    elsif (def_vamp == true)
+      result = "definitely a vampire"
 
-puts "vampire test results: #{result}"
+    # Otherwise, print “Results inconclusive.”
 
-current_count = current_count + 1
+    else
+      result = "results inconclusive"
+    end
+
+    # Print the result at the end of the survey.
+
+  end
+
+  puts "vampire test results: #{result}"
+
+  current_count = current_count + 1
 
 end
 
