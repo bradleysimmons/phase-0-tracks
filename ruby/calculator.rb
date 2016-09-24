a37e7be51dd733e85c39f602128a4d0d83626085
bradley simmons <bradleysimmons9@gmail.com>
2016-09-24 17:14:37 -0500

    interface

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -17,8 +17,22 @@ def calculate(integer, operator, integer_2)
   end
 end
 
-p calculate(4, "+", 5)
-p calculate(4, "-", 5)
-p calculate(4, "*", 5)
-p calculate(4, "/", 5)
+# p calculate(4, "+", 5)
+# p calculate(4, "-", 5)
+# p calculate(4, "*", 5)
+# p calculate(4, "/", 5)
+
+#Comment out your driver code and create a user interface for your program: Prompt the user for a calculation that the user would like to perform, and use your method to give the user a result. For instance, if the user types "4 + 5", the program should print a result of 9 and then exit. You can assume correct input on the user's part.
+
+puts "basic calculator:"
+puts "enter a number"
+integer = gets.chomp
+integer = integer.to_i
+puts "enter a second number"
+integer_2 = gets.chomp
+integer_2 = integer_2.to_i
+p "enter an operation (choices: '+', '-', '*', '/')"
+operator = gets.chomp
+
+p calculate(integer, operator, integer_2)
 
