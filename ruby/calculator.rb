8d91cb66b6019dcd014aadf6c73efca68bc95959
bradley simmons <bradleysimmons9@gmail.com>
2016-09-24 17:24:33 -0500

    add loop for input

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -24,15 +24,34 @@ end
 
 #Comment out your driver code and create a user interface for your program: Prompt the user for a calculation that the user would like to perform, and use your method to give the user a result. For instance, if the user types "4 + 5", the program should print a result of 9 and then exit. You can assume correct input on the user's part.
 
-puts "basic calculator:"
-puts "enter a number"
-integer = gets.chomp
-integer = integer.to_i
-puts "enter a second number"
-integer_2 = gets.chomp
-integer_2 = integer_2.to_i
-p "enter an operation (choices: '+', '-', '*', '/')"
-operator = gets.chomp
+#Update your program to allow the user to do as many calculations as they want (so the user might enter 3 + 4, receive the answer, and then enter 7 - 1 as the next calculation, and so on). When the user types "done" instead of a calculation, the program can exit.
 
-p calculate(integer, operator, integer_2)
+
+until
+  puts "basic calculator: type 'done' to exit"
+  puts "enter a number:"
+  integer = gets.chomp
+    if integer == "done"
+      break
+    else
+      integer = integer.to_i
+    end
+  puts "enter an operation (choices: '+', '-', '*', '/'):"
+  operator = gets.chomp
+    if integer == "done"
+      break
+    end
+  puts "enter a second number:"
+  integer_2 = gets.chomp
+    if integer == "done"
+      break
+    else
+      integer_2 = integer_2.to_i
+    end
+    puts "\n"
+    puts "result: #{calculate(integer, operator, integer_2)}"
+    puts "\n"
+end
+
+
 
