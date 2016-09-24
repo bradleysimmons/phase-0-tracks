c0918f2478431a7ab1b61c3397593863250db932
bradley simmons <bradleysimmons9@gmail.com>
2016-09-24 16:40:19 -0500

    commit assignment

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,205 +1,101 @@
 # write a method that takes an array of integers and an integer to search for. The method should return the index of the item, or nil if the integer is not present in the array. Don't use built-in array methods like .index. You are allowed to use .length and .each.
 
-## PSEUDO CODE
-
-# Method (array of integers, integer to search for)
-#     1. return integer index, or NIL if not present
-#     2. dont use built-in.
-# end
-########################## RELEASE 1 #########################
-########################## METHODS #########################
-
-# def search_array(array, integer)
-#   counter = 0                      # Our own INDEX!
-#   array.each do |value|            # We iterate through array, checking each "value"
-#     if value == integer; return counter; end     # return the index of the "integer" we searched for
-#     counter += 1
-#     if counter >= array.length ; return nil ;end # return nil if "integer" is not present in the array
-#   end
-# end
-
-# ########################## DRIVER CODE release 1 #########################
-# array = [1,2,3,4,5]
-# puts search_array(array, 6)
-
-# ######### DEBUGGING EXAMPLE ##################
-# arr = [42, 89, 23, 1]
-# p "SEARCH ARRAY- should print 3: #{search_array(arr, 1)}"
-# #=> 3
-# p "SEARCH ARRAY- should print nil: #{search_array(arr, 24)}"
-# # => nil
-
-
-# ######################### RELEASE 2 #########################
-
-# def fibonacci(amount)
-#   fib_array = [0, 1]
-#   current_value = 0
-#   if (amount == 0) ; return 0; end # Edge case...
-#   if (amount == 1) ; return 1; end # Edge case...
-
-#   index = 2 # Start indexing after the edge cases.
-#   until index == amount
-#     current_value = fib_array[index - 1] + fib_array[index - 2]   # Equation for Fib-sequence. push it onto array afterwards.
-#       #puts current_value # DEBUG
-#     fib_array.push(current_value)
-#     index += 1 # counter approaching amount
-#   end 
-#   return fib_array[-1] # return the last value we did!
-# end
-
-# ########################## DRIVER release-2 #########################
-
-# puts "Here is your Fibonacci at 100: #{fibonacci(100)} Should be 218922995834555169026"
-
-
-# ########################## RELEASE 3 #########################
-
-# # Bubble Sort Notes: It's a loop within a loop, with the inner loop testing a value against the next value, and swapping them accordingly, then moving to the next pair. The outer loop makes the inner loop run again, until there are no swaps to be made. So in an un-ideal situation, you would unnecessarily run through the loop an excessive amount of times. ("Bubbles to the top.")
-
-# # MergeSort: Splits the array into smaller arrays, sorting them, then merging them together.
-
-
-# # Pseudocode a sorting method that takes an array of integers and uses a sorting algorithm. The method should return a sorted version of the array. Your pseudocode should not be Ruby-specific.
-
-# # Insertion Sort: 
-
-# def insertion_sort(array)
-#     final = [array[0]]        # Final grabs the first value in array.
-#     array.delete_at(0)        # Then it deletes that value, to compare others
-#     # main code
-#     for i in array            # for every integer in array:
-#         final_index = 0
-#         while final_index < final.length     # Both index and length will increase, till length stops increasing, stopping loop.
-#             if i <= final[final_index]       #IF the integer is less than the final_index integer:
-#                 final.insert(final_index,i)  #   Insert the integer into the current index.
-#                 break
-#             elsif final_index == final.length-1 #IF the index reaches the end of final's array, that means the integer is greater than all:
-#                 final.insert(final_index+1,i)   #  Insert the value to the end of array cause it's the greatest value #COULD USE .PUSH HERE
-#                break 
-#             end
-#             final_index+=1                  # We will go to the next index of final, if no conditions are matched
-#         end # END WHILE LOOP
-#     end     # END FOR LOOP
-#     # output
-#     final #returns final array
-# end
-# array = [1, 5, 3, 4, 6, 3] # Expected output (6 length): [1, 3, 3, 4, 5, 6]
-# puts "Insertion Array: #{insertion_sort(array)}" #DEBUG
-
-# LINE BY LINE CODE: ^ = insertions
-# 1 =>  final[1]
-# 5 => satisfies elsif-statement(line75), on the first iteration, resulting in final[1,5^]
-# 3 =>  satisfies 72 (line75), on the second iteration, resulting in final[1, 3^, 5]
-# 4 =>  satisfies 72 (line75), after iterating 3 times. resulting in final[1, 3 , 4^, 5]
-
-
-############################# Can delete the following:
-############## Implement the sorting method in Ruby. ### I CANT DO THIS :( ############################################
-# def mergesort(array)
-#   # this will split them
-#   def mergesort_iter(array_sliced)
-#     return array_sliced if array_sliced.length <= 1 # in case the array sliced would be under 1.
-
-#     mid = array_sliced.length/2 - 1
-#     left_sorted = mergesort_iter(array_sliced[0..mid])
-#                     p "left_sorted: #{left_sorted} " # DEBUG
-#     right_sorted = mergesort_iter(array_sliced[mid+1..-1])
-#                     p "right_sorted: #{right_sorted} " # DEBUG
-#     return merge(left_sorted, right_sorted)
-#   end
-
-#   # this will merge them together
-#   def merge(left_sorted, right_sorted)
-#     res = [] #result
-#     l = 0    # left column
-#     r = 0    # etc
-
-#     loop do
-#       break if r >= right_sorted.length and l >= left_sorted.length
-
-#       if r >= right_sorted.length or (l < left_sorted.length and left_sorted[l] < right_sorted[r])
-#         res << left_sorted[l]
-#         l += 1      # counter 
-#       else
-#         res << right_sorted[r]
-#         r += 1
-#       end
-#     end
-
-#     return res
-#   end
-
-#   mergesort_iter(array) # This is what happens first, in mergesort.
-# end
-
-# p "MergeSort Result: #{mergesort([1,2,3,231,23,51,5,6,16,16,16,1617,7484])} "
-
-#######################################################################################################################
-
-# write a method that takes an array of integers and an integer to search for. The method should return the index of the item, or nil if the integer is not present in the array. Don't use built-in array methods like .index. You are allowed to use .length and .each.
-
-# array = [1, 2, 3, 10, 5, 6, 7, 8, 9, 10]
-# array_two = [1,2,5,4,3,7,10,10]
-
-# def search(array_name, integer)
-#   index_number = 0
-#   search_results = []
-#   array_name.each do |value|
-#       if value == integer
-#         search_results.push(index_number)
-#       end
-#       index_number +=1
-#   end
-#   if search_results.empty?
-#     puts "integer not present"
-#   else
-#     p search_results
-#   end
-# end
-
-
-# search(array, 10)
-# search(array_two, 10)
-
-########################
-
-array = [1, 2, 3, 10, 5, 6, 7, 8, 10, 10]
+array = [1, 2, 3, 4, 3, 2, 1]
 
 
 def search(array_name, integer)
-  
   index_number = 0
+  search_results = []
   array_name.each do |value|
       if value == integer
-        return index_number
+        search_results.push(index_number) 
       end
-      if array_name.length - 1 == index_number
-        return nil
-      end
-      index_number += 1
-      
+      index_number +=1
+  end
+  if search_results.empty?
+    puts "integer not present"
+    return nil
+  else
+    p search_results
   end
 end
 
+search(array, 3)
+search(array, 4)
+p search(array, 5)
 
+################# i wanted to make an interface but it is currently broken
 
+# until
+# puts "input 'q' to quit"
+# puts "array name:"
+# array_name = gets.chomp
+#   if array_name == "q"
+#     break
+#   end
+# puts "integer:"
+# integer = gets.chomp
+# integer = integer.to_i
+#   if integer == "q"
+#     break
+#   end
 
-p search(array, 10)
+# puts array_name
+# p search(array_name, integer)
+# end
 
+# i can take the integer i want to search for with gets.chomp and pass it through fine.  when i take the name of the array i want to search for, i'm pretty sure it tries to evaluate it as a string instead of pointing it to the array with that variable name.  i've placed my array definition with the variable name both inside and outside of the method, but i haven't been able to figure it out.
 
+########################
 
 ##Add a method to your file that takes a number of Fibonacci terms to generate and returns an array of the terms. For example, fib(6) would return [0,1,1,2,3,5]. Your method should work for a large number of terms. To verify your work: the last number in the array generated by fib(100) will be 218922995834555169026. (How can you verify this without having to compare this huge number manually? Be smart with your driver code!)
 
-# def fibonacci(number)
-#   array = [0, 1]
-#   index = 2
-#   until index == number
-#     last_number = array[index - 1] + array[index - 2]
-#     array.push(last_number)
-#     index += 1
-#   end
-#     return array[-1]
-# end
+def fibonacci(number)
+  array = [0, 1]
+  index = 2
+  until index == number
+    last_number = array[index - 1] + array[index - 2]
+    array.push(last_number)
+    index += 1
+  end
+    return array[-1]
+end
 
-# p fibonacci(100)
+p fibonacci(100)
+puts fibonacci(100) == 218922995834555169026
+
+########################
+
+# Choose a simple sorting algorithm to learn about. Bubble sort or insertion sort are good ones for beginners, but you're welcome to be more adventurous.
+# Form a conceptual understanding of how the algorithm works. What helps you grasp it? Do visualizations help? How can you find the right materials for yourself?
+# Notice your emotional state as you tackle this algorithm: do you feel frustrated, underqualified, stupid, or overwhelmed? Or is it more like excitement and curiosity? How do you think these feelings affect your ability to learn?
+# Pseudocode a sorting method that takes an array of integers and uses a sorting algorithm. The method should return a sorted version of the array. Your pseudocode should not be Ruby-specific.
+# Implement the sorting method in Ruby.
+
+def bubble_sort(array)
+  n = array.length   # assigns array length to variable n
+  count = 0
+  loop do
+    swapped = false   # initializes swapped variable as false
+    p "count = #{count}"
+    p "array = #{array}"
+    (n-1).times do |i|  # for array length - 1 iterate through 
+      p "count = #{count}"
+      p "array = #{array}"
+      # p "if #{array[i]} > #{array[i+1]}"
+      if array[i] > array[i+1]    # if preceding number is larger
+        array[i], array[i+1] = array[i+1], array[i] # swap them and run through again
+        swapped = true   # marking that action has been taken
+        count += 1
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+array = [2, 10, 8, 4, 6]
+
+p bubble_sort(array)
