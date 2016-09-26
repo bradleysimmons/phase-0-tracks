607421cc24dc4167ad17c20efde2475f9d3e635a
bradley simmons <bradleysimmons9@gmail.com>
2016-09-26 13:32:21 -0500

    design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+#How might you design a Puppy class? What sorts of characteristics 
+#and behavior might a Puppy class involve? Using a format similar 
+#to the one we used for Eyelashus Adorableus, come up with some 
+#characteristics and behavior for a Puppy class. Use comments 
+#to add your class design to phase-0-tracks/ruby/puppy.rb.
+
+# CLASS
+# puppy
+
+# CHARACTERISTICS (attributes)
+# legs: usually no more than 4
+# tail: usually
+# spoken language: barks, snarls, and whimpers
+# breed: various
+
+# BEHAVIOR (methods)
+# sniff
+# pee on stuff
+# chew on stuff
+# sleep
+# eat
