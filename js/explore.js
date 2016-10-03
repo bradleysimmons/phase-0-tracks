b8a0856c84490f9673ccf4796bb8f7eaa123d6bb
bradley simmons <bradleysimmons9@gmail.com>
2016-10-03 14:37:53 -0500

    add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,20 @@
+// In comments, pseudocode a function that takes a string 
+// as a parameter and reverses the string. reverse("hello"), 
+// for example, should return "olleh". This isn't a task 
+// specific to JavaScript -- your pseudocode should be in 
+// plain English and use phrases like "for each" instead 
+// of JavaScript-specific terms. Remember that you can add 
+// strings in JavaScript, so '' + 'a' would result in the 
+// string 'a'. Take your time. This is the most valuable 
+// skill in programming: being able to think through a 
+// problem logically.
+
+
+// find last character and add to new string
+// find second to last character and add to string
+// build a loop that increments this 
+// and adds to new string until
+// new string and input string length are equal
+
+
+
