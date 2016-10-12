8463c5b194cfb7408d59b1a688887fd23e6cfcba
bradley simmons <bradleysimmons9@gmail.com>
2016-10-12 13:56:42 -0500

    following along with video

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -3,8 +3,8 @@
 # talk about Object Relational Mapping (ORM)
 
 # require gems
-require 'sqlite3'
-require 'faker'
+require "sqlite3"
+require "faker"
 
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
@@ -25,9 +25,9 @@ db.execute(create_table_cmd)
 # add a test kitten
 # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
-# add LOOOOTS of kittens!
-# so. many. kittens. 
-#KittenExplosion
+# # add LOOOOTS of kittens!
+# # so. many. kittens. 
+# #KittenExplosion
 def create_kitten(db, name, age)
   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
 end
@@ -38,7 +38,9 @@ end
 
 # explore ORM by retrieving data
 # kittens = db.execute("SELECT * FROM kittens")
+# puts kittens.class
+# p kittens
 # kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
+ # puts "#{kitten['name']} is #{kitten['age']}"
 # end
 
