adf602f4426d563946f71c1d119439688de0ec03
bradley simmons <bradleysimmons9@gmail.com>
2016-10-18 22:29:09 -0500

    add display menu function

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,18 @@
 console.log("the script is running");
 
-function addPinkBorder(event) {
-  console.log("click event:");
-  console.log(event);
-  event.target.style.border = "2px solid pink";
+// function addPinkBorder(event) {
+//   console.log("click event:");
+//   console.log(event);
+//   event.target.style.border = "2px solid pink";
+// }
+
+// var photo = document.getElementById("");
+// photo.addEventListener("click", addPinkBorder);
+
+
+function displayMenu() {
+  var menu = document.getElementById("nav_menu");
+  menu.style.display = "inline";
 }
 
-var photo = document.getElementById("");
-photo.addEventListener("click", addPinkBorder);
+displayMenu();
