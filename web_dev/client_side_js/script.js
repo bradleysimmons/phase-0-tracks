454be59f40a64e874e92c01582d3c0e333dfd49a
bradley simmons <bradleysimmons9@gmail.com>
2016-10-18 22:49:21 -0500

    add event listener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,18 +1,10 @@
 console.log("the script is running");
 
-// function addPinkBorder(event) {
-//   console.log("click event:");
-//   console.log(event);
-//   event.target.style.border = "2px solid pink";
-// }
-
-// var photo = document.getElementById("");
-// photo.addEventListener("click", addPinkBorder);
-
-
 function displayMenu() {
   var menu = document.getElementById("nav_menu");
   menu.style.display = "inline";
 }
 
-displayMenu();
+var h2 = document.getElementsByTagName("h2");
+h2 = h2[0];
+h2.addEventListener("click", displayMenu);
