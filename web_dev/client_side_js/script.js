7bbbb5c5929fbc8a4e86f03139e361447f264865
bradley simmons <bradleysimmons9@gmail.com>
2016-10-18 21:45:33 -0500

    write index.html, copy script from prompt

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,10 @@
 console.log("the script is running");
+
+function addPinkBorder(event) {
+  console.log("click event:");
+  console.log(event);
+  event.target.style.border = "2px solid pink";
+}
+
+var photo = document.getElementById("");
+photo.addEventListener("click", addPinkBorder);
