d62a0283eb7813f85a40ff390cd69c4bb1650d65
bradley simmons <bradleysimmons9@gmail.com>
2016-10-20 23:34:52 -0500

    refactor contact route

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -9,10 +9,10 @@ db.results_as_hash = true
 # add a query parameter
 ##### http://localhost:9393/?name=brad&age=30
 # GET /
-# get '/' do
-#   name = params[:name]
-#     "#{params[:name]} is #{params[:age]} years old"
-#   end
+get '/' do
+  name = params[:name]
+    "#{params[:name]} is #{params[:age]} years old"
+  end
 
 # write a GET route with
 # route parameters
@@ -50,7 +50,7 @@ end
 ## write a GET route that displays 
 ## an address
 
-get '/' do
+get '/contact/:address' do
   address = params[:address]
   "the address is: #{address}"
 end
